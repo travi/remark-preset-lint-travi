bfb5809fd3fe6ceef8b29f8aa75cedabb672e9d8
Matt Travi <programmer@travi.org>
2018-10-14 21:19:25 -0500

    feat(plugins): added a few more rules

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 exports.plugins = [
   require('remark-preset-lint-recommended'),
-  [require('remark-lint-list-item-indent'), 'space']
+  [require('remark-lint-list-item-indent'), 'space'],
+  require('remark-lint-no-tabs'),
+  require('remark-lint-final-definition'),
+  require('remark-validate-links')
 ];
