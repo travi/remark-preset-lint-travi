d810bb394255533fbf0d5d479aa07f6c2bd43305
Matt Travi <programmer@travi.org>
2018-10-14 23:34:14 -0500

    feat(ordered-list): ensured each item is marked with `1.`

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ exports.plugins = [
   require('remark-lint-no-tabs'),
   require('remark-lint-final-definition'),
   require('remark-validate-links'),
-  [require('remark-lint-maximum-line-length'), 80]
+  [require('remark-lint-maximum-line-length'), 80],
+  [require('remark-lint-ordered-list-marker-value'), 'one']
 ];
