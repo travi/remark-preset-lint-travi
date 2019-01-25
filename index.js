a6d5a4cdfa4d867fba8bc751b23492762a266926
Matt Travi <programmer@travi.org>
2019-01-25 00:14:21 -0600

    feat(no-empty-url): added rule to prevent empty links
    closes #2

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ exports.plugins = [
   require('remark-lint-final-definition'),
   require('remark-validate-links'),
   [require('remark-lint-maximum-line-length'), 80],
-  [require('remark-lint-ordered-list-marker-value'), 'one']
+  [require('remark-lint-ordered-list-marker-value'), 'one'],
+  require('remark-lint-no-empty-url')
 ];
