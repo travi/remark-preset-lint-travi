a8f9d24f20811da6642d8d50ecfb97d0a8845f21
Matt Travi <programmer@travi.org>
2020-04-05 11:23:37 -0500

    fix: extended the form8ion config
    rather than this being the base config
    
    closes form8ion/remark-lint-preset#9

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-exports.plugins = [
-  require('remark-preset-lint-recommended'),
-  [require('remark-lint-list-item-indent'), 'space'],
-  require('remark-lint-no-tabs'),
-  require('remark-lint-final-definition'),
-  require('remark-validate-links'),
-  [require('remark-lint-maximum-line-length'), 80],
-  [require('remark-lint-ordered-list-marker-value'), 'one'],
-  require('remark-lint-no-empty-url'),
-  [require('remark-lint-unordered-list-marker-style'), '*']
-];
+exports.plugins = [require('@form8ion/remark-lint-preset')];
